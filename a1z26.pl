a81a100f1711704c1fa0fcdabbe30ef60d981146
ormuseldesu <9354579+OrmusELdesu@users.noreply.github.com>
2020-11-12 20:15:51 +1100

    allow file handling and command line arguments

--- a1z26.pl
+++ a1z26.pl
@@ -1,7 +1,100 @@
 #!/usr/bin/env perl -w
 # A1Z26 CIPHER
 use strict;
-sub encode 
+
+#import module for parameter processing
+use Getopt::Long qw(GetOptions);
+
+#enable gnu_getopt for combined flags
+Getopt::Long::Configure qw(gnu_getopt);
+
+my $encrypt;
+my $decrypt;
+my $input;
+my $output;
+
+GetOptions(
+   'encrypt|e' => \$encrypt,
+   'decrypt|d' => \$decrypt,
+   'input|i=s'  => \$input,
+   'output|o=s'  => \$output
+) or die "Usage: $0 [--encrypt|-e, --decrypt|-d] [--input | -i <file>] [--output | -o <file>] <string>";
+
+my $result;
+
+if($encrypt) {
+   if($input) {
+      if($output){ #input and output file is specified
+
+         # handle enciphered message file output
+         open (my $fh, '>', $output) or die "Could not open file '$output' $!";
+         $result = file_encode($input);
+         print $fh $result."\n";
+         close($fh);
+
+      } else { #input file specified, output file not specified.
+
+         $result = file_encode($input);
+         print $result."\n";
+
+      }
+   } else { #neither input nor output specified, single string
+      $result = str_encode($ARGV[0]);
+      print $result."\n";
+   }
+} elsif($decrypt) {
+   if($input) {
+      if($output){
+         # handle deciphered message file output
+         open (my $fh, '>', $output) or die "Could not open file '$output' $!";
+         $result = file_decode($input);
+         print $fh $result."\n";
+         close($fh);
+
+
+      } else {
+         $result = file_decode($input);
+         print $result."\n";
+      }
+   } else  {
+      $result = str_decode($ARGV[0]);
+      print $result."\n";
+   }
+}
+
+sub file_encode
+{
+   my ($filename) = @_;
+   my $fe_result = "";
+   open (my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+
+   while(my $row = <$fh>) 
+   {
+      chomp $row;
+      $fe_result .= str_encode($row)."\n";
+   }
+   chop($fe_result);
+   close($fh);
+   return $fe_result;
+}
+
+sub file_decode
+{
+   my ($filename) = @_;
+   my $fd_result = "";
+   open (my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+
+   while(my $row = <$fh>) 
+   {
+      chomp $row;
+      $fd_result .= str_decode($row)."\n";
+   }
+   chop($fd_result);
+   close($fh);
+   return $fd_result;
+}
+
+sub str_encode 
 {
   my ($msg) = @_;
   my $cipher = "";
@@ -19,5 +112,22 @@ sub encode
   return $cipher;
 }
 
-my $result = encode($ARGV[0]);
-print $result."\n";
+sub str_decode
+{
+   my ($cipher) = @_;
+   my $msg = "";
+   for my $i (split /\s/, $cipher)
+   {
+      for my $j (split /-/, $i)
+      {
+         if( $i =~ /(\d+)/ )
+         {
+            $msg .= chr(int($j - 1 + ord('a')));
+         } 
+      }
+      $msg .= " ";
+   }
+   chop($msg);
+   return $msg;
+}
+
